89a0a10cf1808d2ec8be792e0cbced7ccadfff8c
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-03-08 23:21:38 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,27 @@
 import webbrowser
-from bs4 import BeautifulSoup
 import requests
+from src import myFunctions
+from tkinter import *
+
+# initialize tkinter root
+root = Tk()
 
 # Information input
-fname = input('First Name')
-lname = input('Last Name ')
+fnameEntry = Entry()
+fnameEntry.grid(row=0, column=1)
+Label(text='First Name').grid(row=0, column=0)
 
-city = input('City')
+lnameEntry = Entry()
+lnameEntry.grid(row=1, column=1)
+Label(text='Last Name').grid(row=1, column=0)
 
-# Checks the first page for the inmate
-try:
-    url = f'https://thegeorgiagazette.com/{city}/{fname}-{lname}/'
-    response = requests.get(url)
-    result = BeautifulSoup(response.text, 'html.parser')
+countyEntry = Entry()
+countyEntry.grid(row=2, column=1)
+Label(text='County').grid(row=2, column=0)
 
-    # Checks if the page exists
-    try:
-        response.raise_for_status()
+# Search Button
+search = Button(text='Search', command=lambda: myFunctions.runSearch(countyEntry,fnameEntry,lnameEntry))
+search.grid(row=4, column=0, columnspan=2)
 
-    # If the page doesn't exist, it prints an error
-    except requests.exceptions.HTTPError as error:
-        print(error)
-
-    # If the page exists, the url/urls are opened
-    else:
-        webbrowser.open(url)
-        reasonForArrest = result.findAll('p')
-        for reasons in reasonForArrest:
-            for x in reasons:
-                print(x.text)
-
-
-except:
-    pass
-
-# Checks pages 2-10 for the inmate
-for x in range(2, 10):
-    url = f'https://thegeorgiagazette.com/{city}/{fname}-{lname}-{x}/'
-    response = requests.get(url)
-    result = BeautifulSoup(response.text, 'html.parser')
-
-    try:
-        response.raise_for_status()
-
-    except requests.exceptions.HTTPError as error:
-        print(x, error)
-        if x == 10:
-            try:
-                webbrowser.open(f'https://thegeorgiagazette.com/{fname}-{lname}/')
-            except requests.exceptions.HTTPError:
-                print(error)
-
-    else:
-        webbrowser.open(url)
-        reasonForArrest = result.findAll('p')
-
-        print(x)
-        for reasons in reasonForArrest:
-            for x in reasons:
-                print(x.text)
+# Run script
+root.mainloop()
